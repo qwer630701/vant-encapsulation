8da03c4c51fd4bea97db0f08bba814b0a48ac374
SYY <188842798@qq.com>
2021-10-09 13:37:57 +0800

    删除

--- config.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const serverConfig = {
-  TITLE: "手机预订",
-  SERVER: "http://139.9.66.78:17080",
-  FILE: "http://139.9.40.110:28866",
-  WECHATAUTH: "http://localhost:9997/"
-};
